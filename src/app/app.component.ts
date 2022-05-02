5faa71f7bc9499e99599a82dc3b0cc68b74080b3
rajperficient01 <rajl.perficient@gmail.com>
2022-05-02 10:47:19 +0530

    initial commir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'emailform';
+  email: string = "";
+
+
+  onSubmit(){
+    console.log(this.email);
+    // add your own logic for data sending
+  }
 }
